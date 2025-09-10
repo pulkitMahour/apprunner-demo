eb7f43039fed71cd9e5f5d2129e6b0983e848d6b
pulkitMahour <pulkitmahour15jan@gmail.com>
2025-09-11 00:37:52 +0530

    Added S3 code

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const {
-	SecretsManagerClient,
-	GetSecretValueCommand,
-} = require("@aws-sdk/client-secrets-manager");
+const multer = require("multer");
+const path = require("path");
+const {SecretsManagerClient, GetSecretValueCommand} = require("@aws-sdk/client-secrets-manager");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const app = express();
 const port = process.env.PORT || 8080;
 const appName = "apprunner-demo";
 const appEnv = process.env.APP_ENV;
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION });
+const S3_BUCKET = "pulkit-bucket-1";
 
 const secretName = "apprunner/pulkit-demo/config";
 const client = new SecretsManagerClient({ region: process.env.AWS_REGION });
@@ -33,7 +37,8 @@ async function loadSecret() {
 		}
 
 		console.log("Secret loaded from secrets manager");
-	} catch (error) {
+	} 
+	catch (error) {
 		console.error("Error loading secret:", error);
 		process.exit(1);
 	}
@@ -46,34 +51,68 @@ async function startServer() {
 		res.send('ok  try "/pulkit" endpoint');
 	});
 
-	app.get("/", (req, res) => {
+	app.use(express.static(path.join(__dirname, "public")));
+
+	app.get("/api/info", (req, res) => {
 		res.json({
 			name: appName,
 			env: appEnv,
 			secret: appSecretValue
-		});
+		})
+	})
+
+	const upload = multer({storage: multer.memoryStorage(), limits: {fileSize: 5 * 1024 * 1024}});
+
+	app.post("/upload", upload.single("file"), async (req, res) => {
+		try {
+			if (!req.file) {
+				return res.status(400).send("No file uploaded.");
+			}
+			
+			const filename = req.file.originalname.replace(/\s+/g, '-');
+			const key = `${Date.now()}-${filename}`;
+			
+			const putCmd = new PutObjectCommand({
+				Bucket: S3_BUCKET,
+				Key: key,
+				Body: req.file.buffer,
+				ContentType: req.file.mimetype,
+				ServerSideEncryption: "AES256"
+			});
+
+			await s3Client.send(putCmd);
+
+			const getCmd = new GetObjectCommand({
+				Bucket: S3_BUCKET,
+				Key: key
+			});
+
+			const signedUrl = await getSignedUrl(s3Client, getCmd, { expiresIn: 3600 });
+
+			return res.json({key, url: signedUrl});
+		} 
+		catch (error) {
+			console.error("Error uploading file:", error);
+			return res.status(500).json({error: 'Upload failed', details: error.message})
+		}
 	});
 
-	app.get("/pulkit", (req, res) => {
-		res.send(`
-		<div style="font-size: 95px; text-align: center;">
-		<marquee behavior="scroll" direction="left" scrollamount="15">
-			<span style="color: red;">You</span>
-			<span style="color: blue;">have</span>
-			<span style="color: green;">pushed</span>
-			<span style="color: orange;">the</span>
-			<span style="color: purple;">new</span>
-			<span style="color: red;">changes!</span>
-			<span style="color: blue;">Deployment</span>
-			<span style="color: green;">has</span>
-			<span style="color: orange;">been</span>
-			<span style="color: purple;">Successful!!!!!</span>
-		</marquee>
-		</div>
-		<div style="text-align: center; margin-top: 40px;">
-		<a href="https://www.youtube.com" target="_blank">Click here to go to YouTube</a>
-		</div>
-	`);
+	app.get("/files/:key", async (req, res) => {
+		try {
+			const key = req.params.key;
+
+			const getCmd = new GetObjectCommand({
+				Bucket: S3_BUCKET,
+				Key: key
+			});
+			
+			const signedUrl = await getSignedUrl(s3Client, getCmd, { expiresIn: 60 });
+			return res.redirect(signedUrl);
+		} 
+		catch (error) {
+			console.error("Error getting file:", error);
+			return res.status(404).send("File not found");
+		}
 	});
 
 	app.listen(port, () => {
